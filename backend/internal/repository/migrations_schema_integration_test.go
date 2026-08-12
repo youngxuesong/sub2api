9c11a2a3972b9c5627e6a1a7d419a7d4d9193da0
youngxuesong <youngxuesong@gmail.com>
2026-08-12 13:28:03 +0800

    test: assert API key route version default

--- backend/internal/repository/migrations_schema_integration_test.go
+++ backend/internal/repository/migrations_schema_integration_test.go
@@ -130,6 +130,7 @@ func TestMigrationsRunner_IsIdempotent_AndSchemaIsUpToDate(t *testing.T) {
 	// api_keys: key length should be 128
 	requireColumn(t, tx, "api_keys", "key", "character varying", 128, false)
 	requireColumn(t, tx, "api_keys", "route_config_version", "bigint", 0, false)
+	requireColumnDefaultEquals(t, tx, "api_keys", "route_config_version", "1")
 	requireColumn(t, tx, "api_keys", "failover_risk_acknowledged_at", "timestamp with time zone", 0, true)
 
 	var incorrectlyVersionedAPIKeys int
